793007053f1a4da96e0d71073dbb1061e2e541b2
Jose Ayerdis <joseayerdis@gmail.com>
2020-02-14 01:53:22 -0600

    Continuing learning about ACF

--- TimeSeriesAnalyisisAndItsApplication/characteristic_of_time_series.R
+++ TimeSeriesAnalyisisAndItsApplication/characteristic_of_time_series.R
@@ -42,3 +42,40 @@ par(mfrow=c(3,1), mar=c(3,2,2,1), cex.main=1.5)
 plot.ts(cs, main=expression(2*cos(2*pi*t/50+.6*pi)))
 plot.ts(cs + w, main=expression(2*cos(2*pi*t/50+.6*pi) + N(0,1)))
 plot.ts(cs + 5*w, main=expression(2*cos(2*pi*t/50+.6*pi +N(0,25))))
+
+
+# Sample ACF and scatterplots
+
+r = round(acf(soi, 12, plot=FALSE)$acf[-1],3)
+par(mfrow=c(1,3))
+plot(lag(soi,-1), soi); legend('topleft', legend=r[1])
+plot(lag(soi,-6), soi); legend('topleft', legend=r[6])
+plot(lag(soi,-12), soi); legend('topleft', legend=r[12])
+
+# Simulating time series
+
+set.seed(101010)
+x1 = 2*rbinom(11, 1, .5) - 1
+x2 = 2*rbinom(101, 1, .5) - 1
+x3 = 2*rbinom(1001, 1, .5) - 1
+y1 = 5 + filter(x1, sides = 1, filter=c(1, -.7))[-1]
+y2 = 5 + filter(x2, sides = 1, filter=c(1, -.7))[-1]
+y3 = 5 + filter(x3, sides = 1, filter=c(1, -.7))[-1]
+plot.ts(y1, type='s'); plot.ts(y2, type='s'); plot.ts(y3, type='s')
+c(mean(y1), mean(y2), mean(y3))
+
+acf(y1, lag.max=4, plot=FALSE)
+acf(y2, lag.max=4, plot=FALSE)
+acf(y3, lag.max=4, plot=FALSE)
+
+# Manual calculation of ACF per lag 2, 1, 0
+( (sum( (y1[c(-1,-2)]-mean(y1)) * (y1[c(-10,-9)]-mean(y1)) ))/9 )/var(y1)
+( (sum( (y1[-1]-mean(y1)) * (y1[-10]-mean(y1)) ))/9 )/var(y1)
+(sum( (y1-mean(y1))^2 )/9 )/var(y1)
+
+
+
+
+
+
+
