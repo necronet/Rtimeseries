c020afc8e100559407f71823af01a4755f988cc5
Jose Ayerdis <joseayerdis@gmail.com>
2020-02-19 12:14:30 -0600

    Add moving average simulation graph

--- TimeSeriesAnalyisisAndItsApplication/arima_models.R
+++ TimeSeriesAnalyisisAndItsApplication/arima_models.R
@@ -8,4 +8,11 @@ for (i in 1:n) {
   plot( arima.sim(list(order=c(1,0,0), ar=ars[i]), n =100), ylab="X", main=(bquote("AR(1) "~phi==~.(ars[i]))))
 }
 
+# As per docs order refers to:
+# A specification of the non-seasonal part of the ARIMA model: 
+# the three integer components (p, d, q) are the AR order, the degree of differencing, and the MA order.
+par(mfrow=c(2,1))
+plot( arima.sim(list(order=c(0,0,1), ma=.9), n =100), ylab="X", main= expression(MA(1)~~~theta==+.9) )
+plot( arima.sim(list(order=c(0,0,1), ma=-.9), n =100), ylab="X", main= expression(MA(1)~~~theta==-.9) )
+
 
