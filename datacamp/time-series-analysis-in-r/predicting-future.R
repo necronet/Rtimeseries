fe32690b0dd6a6b9b9ba5a10df1844d02d6ad4c1
Jose Ayerdis <joseayerdis@gmail.com>
2020-03-08 17:56:42 -0600

    Finishing section on forecast

--- datacamp/time-series-analysis-in-r/predicting-future.R
+++ datacamp/time-series-analysis-in-r/predicting-future.R
@@ -58,5 +58,19 @@ ts.plot(random_walk)
 abline(0, int_wn)
 
 
+# Use arima.sim() to generate WN data
+white_noise <- arima.sim(model= list(order = c(0,0,0)), n =100)
+  
+# Use cumsum() to convert your WN data to RW
+random_walk <- cumsum(white_noise)
+
+# Use arima.sim() to generate WN drift data
+wn_drift <- arima.sim(model = list(order = c(0,0,0)), n=100, mean = .4)
+  
+# Use cumsum() to convert your WN drift data to RW
+rw_drift <- cumsum(wn_drift)
+  
+# Plot all four data objects
+plot.ts(cbind(white_noise, random_walk, wn_drift, rw_drift))
 
 
