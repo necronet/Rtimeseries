b3935acff6e1d40e978c3d8d8dc8df1b968eef5c
Jose Ayerdis <joseayerdis@gmail.com>
2020-03-30 10:28:30 -0600

    Exponential smoothing automatic model selection

--- datacamp/forecasting_in_R/exponential-smoothing.R
+++ datacamp/forecasting_in_R/exponential-smoothing.R
@@ -66,3 +66,42 @@ accuracy(fcsn, hyndsight)
 
 # Plot the better forecasts
 autoplot(fchw)
+
+# Automatic model selection with ETS
+
+# Fit ETS model to austa in fitaus
+fitaus <- ets(austa)
+
+# Check residuals
+checkresiduals(fitaus)
+
+# Plot forecasts
+autoplot(forecast(fitaus))
+
+# Repeat for hyndsight data in fiths
+fiths <- ets(hyndsight)
+checkresiduals(fiths)
+autoplot(forecast(fiths))
+
+# Which model(s) fails test? (TRUE or FALSE)
+fitausfail <- FALSE
+fithsfail <- TRUE
+
+# Function to return ETS forecasts
+fets <- function(y, h) {
+  forecast(ets(y), h = h)
+}
+
+
+# Apply tsCV() for both methods
+e1 <- tsCV(qcement, fets, h = 4)
+e2 <- tsCV(qcement, snaive, h = 4)
+
+# Compute MSE of resulting errors (watch out for missing values)
+mean(e1^2, na.rm = TRUE)
+mean(e2^2, na.rm = TRUE)
+
+# Copy the best forecast MSE
+bestmse <- e1
+
+
