82794ae0799583e390a40caf394977a9a3115986
Jose Ayerdis <joseayerdis@gmail.com>
2020-03-31 00:02:12 -0600

    Add dynamic harmonic regression with fourier transform

--- datacamp/forecasting_in_R/advanced-methods.R
+++ datacamp/forecasting_in_R/advanced-methods.R
@@ -32,3 +32,51 @@ fit <- auto.arima(elec[, "Demand"], xreg = xreg)
 forecast(fit, xreg = cbind(MaxTemp=20, MaxTempSq=20^2, Workday=1), h =1)
 
 
+# using harmonic dynamic regression
+
+# Set up harmonic regressors of order 13
+harmonics <- fourier(gasoline, K = 13)
+
+
+# Fit regression model with ARIMA errors
+fit <- auto.arima(gasoline, xreg = harmonics, seasonal = FALSE)
+
+# Forecasts next 3 years
+newharmonics <- fourier(gasoline, K = 13, h = 3*52)
+fc <- forecast(fit, xreg = newharmonics)
+
+# Plot forecasts fc
+autoplot(fc)
+
+
+# Fit a harmonic regression using order 10 for each type of seasonality
+fit <- tslm(taylor ~ fourier(taylor, K = c(10, 10)))
+
+# Forecast 20 working days ahead
+fc <- forecast(fit, newdata = data.frame(fourier(taylor, K = c(10,10), h = 48*20)))
+
+# Plot the forecasts
+autoplot(fc)
+
+# Check the residuals of fit
+checkresiduals(fit)
+
+
+# Plot the calls data
+autoplot(calls)
+
+# Set up the xreg matrix
+xreg <- fourier(calls, K = c(10,0))
+
+# Fit a dynamic regression model
+fit <- auto.arima(calls, xreg = xreg, seasonal = FALSE, stationary = TRUE)
+
+# Check the residuals
+checkresiduals(fit)
+
+# Plot forecasts for 10 working days ahead
+fc <- forecast(fit, xreg =  fourier(calls, c(10, 0), h = 1690))
+autoplot(fc)
+
+
+
