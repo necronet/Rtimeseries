ffa81f910a0b83ef59c70102be9ea44b73f07464
Jose Ayerdis <joseayerdis@gmail.com>
2020-03-31 01:23:06 -0600

    finish TBATS example

--- datacamp/forecasting_in_R/advanced-methods.R
+++ datacamp/forecasting_in_R/advanced-methods.R
@@ -79,4 +79,22 @@ fc <- forecast(fit, xreg =  fourier(calls, c(10, 0), h = 1690))
 autoplot(fc)
 
 
+# TBATS
+
+# Plot the gas data
+autoplot(gas)
+
+# Fit a TBATS model to the gas data
+fit <- tbats(gas)
+
+# Forecast the series for the next 5 years
+fc <- forecast(fit, h = 5*12)
+
+# Plot the forecasts
+autoplot(fc)
+
+# Record the Box-Cox parameter and the order of the Fourier terms
+lambda <- 0.0852
+K <- 5
+
 
