9583669a39c052ff13ac05e32efe60a2ad3c7e3a
Jose Ayerdis <joseayerdis@gmail.com>
2020-04-07 02:49:15 -0600

    Finish on advanced forecasting methods

--- ForecastingPrinciplesAndPractice/advanced_methods.R
+++ ForecastingPrinciplesAndPractice/advanced_methods.R
@@ -1,3 +1,7 @@
+library(fpp2)
+library(forecast)
+library(vars)
+
 p1 <- autoplot(calls) +
   ylab("Call volume") + xlab("Weeks") +
   scale_x_continuous(breaks=seq(1,33,by=2))
@@ -47,3 +51,46 @@ fc <- forecast(fit,
 autoplot(fc, include=14*48)
 
 checkresiduals(fc)
+
+
+# Vector autoregressive model
+
+VARselect(uschange[,1:2], lag.max=8, type="const")[["selection"]]
+
+var1 <- VAR(uschange[,1:2], p=1, type="const")
+serial.test(var1, lags.pt=10, type="PT.asymptotic")
+var2 <- VAR(uschange[,1:2], p=2, type="const")
+serial.test(var2, lags.pt=10, type="PT.asymptotic")
+
+var3 <- VAR(uschange[,1:2], p=3, type="const")
+serial.test(var3, lags.pt=10, type="PT.asymptotic")
+
+forecast(var3) %>% autoplot() + xlab("Year")
+
+
+fit <- nnetar(sunspotarea, lambda=0)
+autoplot(forecast(fit,h=30, PI=TRUE))
+
+
+sim <- ts(matrix(0, nrow=30L, ncol=9L),
+          start=end(sunspotarea)[1L]+1L)
+for(i in seq(9))
+  sim[,i] <- simulate(fit, nsim=30L)
+
+autoplot(sunspotarea) + autolayer(sim)
+
+
+
+
+
+df <- data.frame(runif(3), runif(3))
+names(df) <- c(1, 2)
+df$`3` = df$`1`+ df$`2`
+
+
+bootseries <- bld.mbb.bootstrap(debitcards, 10) %>%
+  as.data.frame() %>% ts(start=2000, frequency=12)
+autoplot(debitcards) +
+  autolayer(bootseries, colour=TRUE) +
+  autolayer(debitcards, colour=FALSE) +
+  ylab("Bootstrapped series") + guides(colour="none")
